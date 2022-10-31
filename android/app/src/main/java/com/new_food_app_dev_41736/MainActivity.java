22b0bd56ba3d5ecccdb420c5e878f9b31a94c6d2
Crowdbotics <engineering@crowdbotics.com>
2022-10-31 13:49:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/new_food_app_dev_41736/MainActivity.java
@@ -0,0 +1,15 @@
+package com.new_food_app_dev_41736;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "new_food_app_dev_41736";
+  }
+}
